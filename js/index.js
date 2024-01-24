4131d595cc10c48af6d0ec6152005f0b71007ab3
vishal gohil <info.vishalgohil@gmail.com>
2024-01-24 19:03:45 +0530

    plan pricing

--- js/index.js
+++ js/index.js
@@ -5,10 +5,10 @@ const emailPattern = /^[\w-\.]+@(myshopify.com)$/
 
 // function onSubmit() {
 //     let emailEvent = document.getElementById('email')
-    
+
 //     // value 
 //     let email = emailEvent.value
-    
+
 //     // regx 
 //     const emailPattern = /^[\w-\.]+@(myshopify.com)$/
 //     if (email.match(emailPattern)) {
@@ -26,31 +26,31 @@ const emailPattern = /^[\w-\.]+@(myshopify.com)$/
 
 function getIcons(icon) {
     switch (icon) {
-        case "01_notifications" :
+        case "01_notifications":
             return "bi-bell-fill"
             break;
-        case "02_impressions_of_products" :
+        case "02_impressions_of_products":
             return "bi-box-fill"
             break;
-        case "03_Unlimited_back_in_stock_requests" :
+        case "03_Unlimited_back_in_stock_requests":
             return "bi-info-circle-fill"
             break;
-        case "04_automatic_back_in_stock_reminders" :
+        case "04_automatic_back_in_stock_reminders":
             return "bi-hourglass-split"
             break;
-        case "05_fully_customizable" :
+        case "05_fully_customizable":
             return "bi-grid-1x2-fill"
             break;
-        case "06_send_notification_to_individual_customer" :
+        case "06_send_notification_to_individual_customer":
             return "bi-person-fill-exclamation"
             break;
-        case "07_online_support" :
+        case "07_online_support":
             return "bi-person-fill-gear"
             break;
-        case "08_basic_reports" :
+        case "08_basic_reports":
             return "bi-file-earmark-text-fill"
             break;
-        default :
+        default:
             return "bi-database-fill-check"
     }
 }
@@ -60,7 +60,7 @@ function getPlanFeatures(val) {
     let count = 0
     Object.entries(val).forEach(([key, plans]) => {
 
-        html+= `<li class="mb-2 d-flex flex-row">
+        html += `<li class="mb-2 d-flex flex-row">
             <div class="pe-3">
                 <i class="bi ${getIcons(key)}"></i>
             </div>
@@ -73,9 +73,9 @@ function getPlanFeatures(val) {
 
 function render(data) {
     let plans = document.getElementById('plans')
-    let html = '' ;
+    let html = '';
     data.forEach(item => {
-        html+= `<div class="col-12 col-md-6 col-lg-4 mb-3 mb-md-4 mb-lg-0 " >
+        html += `<div class="col-12 col-md-6 col-lg-4 mb-3 mb-md-4 mb-lg-0 " >
                 <div class="custom-card h-100" >
                     <div class="d-flex flex-column justify-content-center align-items-center border-bottom pb-3">
                         <div class="my-3">
@@ -88,7 +88,7 @@ function render(data) {
                             ${getPlanFeatures(item.plan_features)}
                         </ul>
                         <div class="d-flex justify-content-center">
-                                <button class="plan-btn secondary-btn">Buy Now</button>
+                                <a href="https://apps.shopify.com/notifyme" style="text-decoration:none;" class="plan-btn secondary-btn">Select</a>
                         </div>
                     </div>
                 </div>
